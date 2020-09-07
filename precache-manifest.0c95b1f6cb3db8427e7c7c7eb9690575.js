0a48fefecab4eb53b7e1ff101abc809c6824fad8
Suraj Verma <surajverma@rentalcars.com>
2020-09-07 21:46:36 +0100

    Updates

---
+++ precache-manifest.0c95b1f6cb3db8427e7c7c7eb9690575.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c7f58686b0cc9c039ab2fa5f8673e6db",
+    "url": "/google-books-client-1/index.html"
+  },
+  {
+    "revision": "7fddd4d87d2b9ef114bc",
+    "url": "/google-books-client-1/static/js/2.cb3f86db.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/google-books-client-1/static/js/2.cb3f86db.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b2610bc534795d50373e",
+    "url": "/google-books-client-1/static/js/main.be00a865.chunk.js"
+  },
+  {
+    "revision": "eeab31034af66e570e7c",
+    "url": "/google-books-client-1/static/js/runtime-main.ac467c12.js"
+  }
+]);
